3a73f1282eb6a119f6549145e8db30b67fa065ad
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-06-20 10:47:35 +0300

    Update divide.php

--- 001cycles_arrays/divide.php
+++ 001cycles_arrays/divide.php
@@ -1,5 +1,28 @@
 <?
 // написать функцию делящую пополам между 2мя числовыми массивами полученный массив случайных чисел
 // считающую и сравнивающую сумму чисел в каждом получившемся массиве
+function SplitHalfUniqueArr($arr) {
 
+  //работаем с массивом со стандартной функцией
+  $length = count($arr); //находим длину массива
+  $middle = count($arr) / 2; //находим середину массива
+
+ 
+  $left_side_arr = $right_side_arr = [];
+  for($i = 0; $i < $middle; $i++ ) {//находим левую часть массива
+    $left_side_arr[] = $arr[$i];//создаем массив с левой частью массива функции $adds_unique_num_arr
+  }
+  echo '<pre>';
+  print_r($left_side_arr);
+  echo '</pre>';
+
+  for($i = $middle + 1; $i <= $length; $i++ ) {//находим правую часть 
+    $right_side_arr[] = $arr[$i];//создаем массив с правой частью массива функции $adds_unique_num_arr
+  }
+  echo '<pre>';
+  print_r($right_side_arr);
+  echo '</pre>';
+
+}
+SplitHalfUniqueArr($adds_unique_num_arr);
 ?>
