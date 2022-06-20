3753b24c51a847b0495c45c209264276f5b1ed4a
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-06-20 10:49:19 +0300

    Update create_random.php

--- 001cycles_arrays/create_random.php
+++ 001cycles_arrays/create_random.php
@@ -1,15 +1,11 @@
 <?
-echo 'Создать массив из случайных уникальных целых чисел скажем от 0 до 200 - 6 уникальных чисел' . '<br>';
-//в цикле добавить счетчик сколько случайных чисел было создано
-//Разделить эти числа на 2 массива, так чтобы сумма чисел в каждом массиве была равна или если это невозможно, то разница между суммами массивов была минимальна
- 
 //Cоздать одно случайное целое число от 1 до 200
 $min = 0; //мин. число
 $max = 200;//макс. число
 $amount = 7; //количество уникальных чисел для значения в массив 
 
 //вариант со стандартной функцией 
-function addsUniqueNumberArray($amount_unique_num, $min, $max) {
+function addsUniqueNumArr($amount_unique_num, $min, $max) {
   $arr_rand = []; //массив случайных чисел 
   $count_num = 0; //счетчик сколько случайных чисел
   $i = 0;
@@ -34,7 +30,7 @@ function addsUniqueКeyArray($amount_unique_num, $min, $max) {
     $rand_num = mt_rand($min, $max);
     
     if(!array_key_exists($rand_num, $arr_rand)){ //проверяем на уникальность ключей 
-      $arr_rand[$rand_num]++;
+      $arr_rand[$rand_num] = $i;
       $i++; 
     }
     
@@ -43,13 +39,17 @@ function addsUniqueКeyArray($amount_unique_num, $min, $max) {
   return $arr_rand;
 }
 
-//вариант со стандартной функцией
+
+$adds_unique_num_arr = addsUniqueNumArr($amount, $min, $max);//вариант со стандартной функцией
+$adds_unique_key_arr  = addsUniqueКeyArray($amount, $min, $max); //вариант с уникальным ключем
+
 echo '<pre>';
-print_r(addsUniqueNumberArray($amount, $min, $max));
+echo 'Вариант со стандартной функцией' . '<br>';
+print_r($adds_unique_num_arr);
 echo '</pre>';
 
-//вариант с уникальным ключем 
 echo '<pre>';
-print_r(addsUniqueКeyArray($amount, $min, $max));
+echo 'Вариант с уникальным ключем' . '<br>';
+print_r($adds_unique_key_arr);
 echo '</pre>';
 ?>
