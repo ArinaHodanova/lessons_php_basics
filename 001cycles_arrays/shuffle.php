cb66b3ffb2834455802a8b295e38ae9670b072bf
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-07-04 10:42:54 +0300

    Update shuffle.php

--- 001cycles_arrays/shuffle.php
+++ 001cycles_arrays/shuffle.php
@@ -1,6 +1,5 @@
 <?
 //Функция должна перетасовать входящий массив и вернуть наиболее сбалансированные выходяшие
-
 function  shuffleArray($num, $arr) {
   if (!is_array($arr)) return $arr;
 
@@ -10,8 +9,7 @@ function  shuffleArray($num, $arr) {
 
   while($i <= $num) {
     shuffle($arr); //перемешиваем массив при каждой итерации 
-
-    echo 'Массив с которым работаем';
+    
     echo '<pre>';
     print_r($arr);
     echo '</pre>';
@@ -27,31 +25,36 @@ function  shuffleArray($num, $arr) {
     //получаем сумму значений массивов
     $left_arr_value_sum = array_sum($left_arr);
     $right_arr_value_sum = array_sum($right_arr);
- 
+
     //получаем разность сумм значений массивов
     $difference = $left_arr_value_sum - $right_arr_value_sum;
+    $difference_arr[] = $difference;//разность сумм значений массивов записываем вновый массив 
     
-    $difference_arr[] = $difference;
+    //вычисляем наиболее близкое к 0 число 
+    $max_value_arr = [];//массив с положит. значениями 
+    $min_value_arr = [];//массив с отриц.  значениями 
+    foreach($difference_arr as $value) {
+      if($value >= 0) {
+        $max_value_arr[] = $value;
+      } 
+      if($value < 0) {
+        $min_value_arr[] = $value;
+      } 
+    }
+
+    //если массив с положительными значениями не пустой
+    if(!empty($max_value_arr)) {
+      $positive_value = min($max_value_arr);//находит самое мин число среди положительных 
+    }
+    //если массив с отриц значениями не пустой
+    if(!empty($min_value_arr)) {
+      $negative_value = max($min_value_arr);//находим самое мин число среди отрицательных
+    }
+
     $i++;
   }
   
-  //вычисляем наиболее близкое к 0 число 
-  $max_value_arr = [];//массив с положит. значениями 
-  $min_value_arr = [];//массив с отриц.  значениями 
-
-  foreach($difference_arr as $value) {
-    if($value >= 0) {
-      $max_value_arr[] = $value;
-    } 
-    if($value < 0) {
-      $min_value_arr[] = $value;
-    } 
-  }
-
-  $positive_value = min($max_value_arr);//находит самое мин число среди положительных 
-  $negative_value = max($min_value_arr);//находим самое мин число среди отрицательных
-  echo $negative_value;
-
 }
+
 $shuffle_array = shuffleArray(3, $adds_unique_num_arr);
 ?>
