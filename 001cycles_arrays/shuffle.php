6b0334616d6f1f9fc19a0fbf97aaaa82015f1e8f
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-07-04 14:55:46 +0300

    Update shuffle.php
    функция делящая пополам между 2мя числовыми массивами полученный массив случайных чисел

--- 001cycles_arrays/shuffle.php
+++ 001cycles_arrays/shuffle.php
@@ -1,60 +1,24 @@
 <?
-//Функция должна перетасовать входящий массив и вернуть наиболее сбалансированные выходяшие
-function  shuffleArray($num, $arr) {
-  if (!is_array($arr)) return $arr;
+// написать функцию делящую пополам между 2мя числовыми массивами полученный массив случайных чисел
+function SplitHalfUniqueArr($arr) {
+  //работаем с массивом со стандартной функцией
+  $length = count($arr); //находим длину массива
+  $middle = count($arr) / 2; //находим середину массива
 
-  $middle = count($arr) / 2;//находим середину массива
-  $i = 1;
-  $difference_arr = [];//масив, который будет накапливать значения, полученные от сумм массива
+  $left_side_unique_arr = [];
+  $right_side_unique_arr = [];
 
-  while($i <= $num) {
-    shuffle($arr); //перемешиваем массив при каждой итерации 
-    
-    echo '<pre>';
-    print_r($arr);
-    echo '</pre>';
-
-    $right_arr_value_sum = 0; //сумма массива правой части
-    $left_arr_value_sum = 0; //сумма массива левой части
-    
-    //делим массив напополам
-    $right_arr = array_slice($arr, $middle, $middle);
-    $left_arr = array_slice($arr, 0, $middle);
-    $difference = 0;
-    
-    //получаем сумму значений массивов
-    $left_arr_value_sum = array_sum($left_arr);
-    $right_arr_value_sum = array_sum($right_arr);
-
-    //получаем разность сумм значений массивов
-    $difference = $left_arr_value_sum - $right_arr_value_sum;
-    $difference_arr[] = $difference;//разность сумм значений массивов записываем вновый массив 
-    
-    //вычисляем наиболее близкое к 0 число 
-    $max_value_arr = [];//массив с положит. значениями 
-    $min_value_arr = [];//массив с отриц.  значениями 
-    foreach($difference_arr as $value) {
-      if($value >= 0) {
-        $max_value_arr[] = $value;
-      } 
-      if($value < 0) {
-        $min_value_arr[] = $value;
-      } 
+  //делим массив с рандомными значениями на два массива. И находим сумму значений каждого полученного массива
+  for($i = 0; $i < $length; $i++ ) {
+    if($i < $middle) { 
+        $left_side_unique_arr[$i] = $arr[$i];//в новый массив выводим значения левой части массива с рандомными значениями (функция $adds_unique_num_arr)
+    } elseif( $i >= $middle) {
+        $right_side_unique_arr[] = $arr[$i];//в новый массив выводим значения правой части массива с рандомными значениями (функция $adds_unique_num_arr)
     }
-
-    //если массив с положительными значениями не пустой
-    if(!empty($max_value_arr)) {
-      $positive_value = min($max_value_arr);//находит самое мин число среди положительных 
-    }
-    //если массив с отриц значениями не пустой
-    if(!empty($min_value_arr)) {
-      $negative_value = max($min_value_arr);//находим самое мин число среди отрицательных
-    }
-
-    $i++;
   }
-  
-}
 
-$shuffle_array = shuffleArray(3, $adds_unique_num_arr);
+  $examination_arr = [$left_side_unique_arr, $right_side_unique_arr];//сформируем массив для проверки 
+  return  $examination_arr;
+}
+$split_half_unique_arr = SplitHalfUniqueArr($adds_unique_num_arr);
 ?>
