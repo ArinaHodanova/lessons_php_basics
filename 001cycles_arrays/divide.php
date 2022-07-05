e03382111bbb360169395ebfa3294e6f342f596a
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-07-05 14:48:25 +0300

    Update divide.php
    Изменила код, который разделяем массив на две части

--- 001cycles_arrays/divide.php
+++ 001cycles_arrays/divide.php
@@ -2,22 +2,14 @@
 // написать функцию делящую пополам между 2мя числовыми массивами полученный массив случайных чисел
 function SplitHalfUniqueArr($arr) {
   //работаем с массивом со стандартной функцией
-  $length = count($arr); //находим длину массива
+
   $middle = count($arr) / 2; //находим середину массива
 
-  $left_side_unique_arr = [];
-  $right_side_unique_arr = [];
+  //делим массив $arr на две части 
+  $right_arr = array_slice($arr, $middle, $middle);
+  $left_arr = array_slice($arr, 0, $middle);
 
-  //делим массив с рандомными значениями на два массива. И находим сумму значений каждого полученного массива
-  for($i = 0; $i < $length; $i++ ) {
-    if($i < $middle) { 
-        $left_side_unique_arr[$i] = $arr[$i];//в новый массив выводим значения левой части массива с рандомными значениями (функция $adds_unique_num_arr)
-    } elseif( $i >= $middle) {
-        $right_side_unique_arr[] = $arr[$i];//в новый массив выводим значения правой части массива с рандомными значениями (функция $adds_unique_num_arr)
-    }
-  }
-
-  $examination_arr = [$left_side_unique_arr, $right_side_unique_arr];//сформируем массив для проверки 
+  $examination_arr = [$right_arr, $right_arr];//сформируем массив для проверки 
   return  $examination_arr;
 }
 $split_half_unique_arr = SplitHalfUniqueArr($adds_unique_num_arr);
