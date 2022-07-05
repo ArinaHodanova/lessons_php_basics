321d6b4f7779119b850eb12366585afb7ede76ad
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-07-05 16:04:54 +0300

    Update shuffle.php

--- 001cycles_arrays/shuffle.php
+++ 001cycles_arrays/shuffle.php
@@ -5,28 +5,30 @@ function  shuffleArray($num, $arr) {
 
   $middle = count($arr) / 2;//находим середину массива
   $i = 1;
-  $difference_arr = [];//масив, который будет накапливать значения, полученные от сумм массива
+  $difference_arr = [];//накапиаем разность полученную от сумм левой и правой части массивов 
 
   while($i <= $num) {
     shuffle($arr); //перемешиваем массив при каждой итерации 
-
+    
     $right_arr_value_sum = 0; //сумма массива правой части
-    $left_arr_value_sum = 0; //сумма массива левой части
-    
+    $left_arr_value_sum = 0; //сумма массива левой части    
+    $difference = //разноть полученнная от ссумм  правой части и  левой части массива;
+
     //делим массив напополам
-    $right_arr = array_slice($arr, $middle, $middle);
     $left_arr = array_slice($arr, 0, $middle);
-    $difference = 0;
-    
+    $right_arr = array_slice($arr, $middle, $middle);
+
     //получаем сумму значений массивов
     $left_arr_value_sum = array_sum($left_arr);
     $right_arr_value_sum = array_sum($right_arr);
 
     //получаем разность сумм значений массивов
     $difference = $left_arr_value_sum - $right_arr_value_sum;
-    $i++;
+    $difference_arr[] = abs($difference);
+    $i++; 
   }
+  return $difference_arr;//абсолютное значение разности выходящего массива
 }
 
-$shuffle_array = shuffleArray(3, $adds_unique_num_arr);
+$shuffle_array = shuffleArray(2, $adds_unique_num_arr);
 ?>
