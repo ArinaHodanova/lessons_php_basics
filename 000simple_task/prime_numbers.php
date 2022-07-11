dd6a68f21545efb2ea0713118a2c8d6e6b4b80c2
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-07-11 15:57:45 +0300

    Update prime_numbers.php

--- 000simple_task/prime_numbers.php
+++ 000simple_task/prime_numbers.php
@@ -1,17 +1,24 @@
 <?php
-//определаем яляется ли переданное число простым 
-   function definePrimeNumbers(int $num) {
-        $arr = [];
-        $flag = true;
-        for ($i = 2; $i < $num; $i++) {
-             if ($num % $i === 0) {
-                  $flag = false;
-                  break;
-              } else {
-                  $flag = true;
-              }
-         }
-         return $flag;
-    }
-        echo definePrimeNumbers(9);
+        //определяем простые числа 
+        function definePrimeNumbers(int $num) {
+            $arr = [];
+            $flag = true;
+            for ($i = 2; $i < $num; $i++) {
+                if ($num % $i === 0) {
+                    $flag = false;
+                    break;
+                } else {
+                    $flag = true;
+                }
+            }
+            return $flag;
+        } 
+
+        $prime_num_arr = [];//массив простых чисел 
+        //передаем набор чисел и выводим в массив простые числа
+        for($i = 2; $i <= 100; $i++) {
+            if(definePrimeNumbers($i) == true) {
+                $prime_num_arr[] = $i;
+            }
+        }
 ?>
