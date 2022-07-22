2e6af89026a9f4bff37a50388324a7c875560a6f
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-07-22 09:07:32 +0300

    Update shuffle.php
    Применила существующие функции по разделению массива на две части, нахождения суммы каждой части

--- 001cycles_arrays/shuffle.php
+++ 001cycles_arrays/shuffle.php
@@ -1,33 +1,28 @@
 <?
 //Функция должна перетасовать входящий массив и возвращает наиболее сбалансированные выходяшие
 function  shuffleArray($num, $arr) {
+function  shuffleArray($num, $arr) {
   if (!is_array($arr)) return $arr;
-
-  $middle = count($arr) / 2;//находим середину массива
+  
   $i = 1;
-  $difference_arr = [];//накапиаем разность полученную от сумм левой и правой части массивов 
-
   while($i <= $num) {
-    shuffle($arr); //перемешиваем массив при каждой итерации 
-    
-    $right_arr_value_sum = 0; //сумма массива правой части
-    $left_arr_value_sum = 0; //сумма массива левой части    
-    $difference = //разноть полученнная от ссумм  правой части и  левой части массива;
+    $shuffle_arr = shuffle($arr); //перемешиваем массив при каждой итерации 
+    echo '<pre>' . 'перемешиваем';
+    print_r($arr);
+    echo '</pre>'; 
 
-    //делим массив напополам
-    $left_arr = array_slice($arr, 0, $middle);
-    $right_arr = array_slice($arr, $middle, $middle);
+    //делим перемешанный вариант массива на две части
+    $divide_half_shuffle_arr = SplitHalf($arr);
+    echo '<pre>' . 'делим пополам';
+    print_r($divide_half_shuffle_arr);
+    echo '</pre>'; 
 
-    //получаем сумму значений массивов
-    $left_arr_value_sum = array_sum($left_arr);
-    $right_arr_value_sum = array_sum($right_arr);
-
-    //получаем разность сумм значений массивов
-    $difference = $left_arr_value_sum - $right_arr_value_sum;
-    $difference_arr[] = abs($difference);
-    $i++; 
+    //находим сумму частей 
+    echo '<pre>' . 'сумма массива';
+    print_r(multiArrSumKeys($divide_half_shuffle_arr));
+    echo '</pre>';
+    $i++;
   }
-  return $difference_arr;//абсолютное значение разности выходящего массива
 }
 
 $shuffle_array = shuffleArray(2, $adds_unique_num_arr);
