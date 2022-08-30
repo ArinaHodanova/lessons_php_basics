55a0f7f35e33d9ed9908051bde481e8a673f1e48
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-08-30 16:32:21 +0300

    Update MyFormulaCalculation.class.php

--- 002cycles_arrays_OOП/MyFormulaCalculation.class.php
+++ 002cycles_arrays_OOП/MyFormulaCalculation.class.php
@@ -3,8 +3,8 @@ class MyFormulaCalculation {
      public static $arr_formulas = [];//для объектов этого класса по имени
 
      //принимает формулы
-     public function fnCreate(string $fn) {
-          return false; 
+     public function fnCreate($fn) {
+          return is_numeric($fn);  
      } 
 
      public function fnResult() {
