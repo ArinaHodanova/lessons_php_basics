1f369b487b0885784ee0f5b4ec73059b4f614420
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-01 14:38:45 +0300

    Update LinkCalculation.class.php
    Нашли ссылки на переменные

--- 002cycles_arrays_OOП/LinkCalculation.class.php
+++ 002cycles_arrays_OOП/LinkCalculation.class.php
@@ -2,18 +2,29 @@
 class LinkCalculation extends MyFormulaCalculation {
   protected $l_var = null;
 
-  public function fnCreate($fn) {
-    $out = parent::fnCreate($fn); 
-    return $out;
+  public function fnCreate($expr) {
+    if (parent::fnCreate($expr)) return true;
+    if(!$this->fnGetLeftAction($expr)) {
+      $this->l_var = $fn;
+      return true; 
+    }
   } 
 
   public function fnResult() {
     $out = parent::fnResult(); 
-    return 77;
+    return $out;
   }
 
-  protected function fnGetLeftAction() {
+  //Находим самый левый ключ операции
+  function fnGetLeftAction(string $exp) {
+    $arr = str_split($exp);
+    $actions = [ '*' => 1, '/' => 1, '+' => 1, '-' => 1 ];
+    foreach($arr as $value) {
+        if (array_key_exists($value, $actions)) {
+            return trim($value);
+        } 
+    }
   }
-  
+
 }
 ?>
