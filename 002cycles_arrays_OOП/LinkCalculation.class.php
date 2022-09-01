c894eaac61a99024dafbd71d7366e79347094a2f
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-01 16:49:32 +0300

    Update LinkCalculation.class.php

--- 002cycles_arrays_OOП/LinkCalculation.class.php
+++ 002cycles_arrays_OOП/LinkCalculation.class.php
@@ -4,24 +4,28 @@ class LinkCalculation extends MyFormulaCalculation {
 
   public function fnCreate($expr) {
     if (parent::fnCreate($expr)) return true;
-    if(!$this->fnGetLeftAction($expr)) {
-      $this->l_var = $fn;
+    if(!$this->fnGetLeftAction(trim($expr))) {
+      $this->l_var = $expr;
       return true; 
     }
   } 
 
   public function fnResult() {
-    $out = parent::fnResult(); 
-    return $out;
+    if(!is_null($this->l_var) && array_key_exists($this->l_var, self::$arr_formulas))  {
+      self::$arr_formulas[$this->l_var];
+      //return $this->fnResult();
+    } else {
+      return parent::fnResult();
+    }
   }
 
   //Находим самый левый ключ операции
   function fnGetLeftAction(string $exp) {
     $arr = str_split($exp);
-    $actions = [ '*' => 1, '/' => 1, '+' => 1, '-' => 1 ];
+    global $actions;//знаки арифмет. действий
     foreach($arr as $value) {
         if (array_key_exists($value, $actions)) {
-            return trim($value);
+            return $value;
         } 
     }
   }
