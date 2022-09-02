3d05a81f9a182dbbc895ee787d6108bd6515703f
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-02 17:54:38 +0300

    Update anonymousCall.php
    anonymous call

--- 000simple_task/anonymousCall.php
+++ 000simple_task/anonymousCall.php
@@ -1,1 +1,15 @@
+<?
+$actions = [ 
+  '*' => 1, 
+  '/' => 1, 
+  '+' => function (int $value = null, int $exp = null) {
+    return $value . ' - ' . $exp ;
+  }, 
+  '-' => function (int $value = null, int $exp = null) {
+    return $value . ' - ' . $exp ;
+  }
+];
 
+echo call_user_func($actions['+'], 10, 20);
+echo call_user_func($actions['-'], 1100, 60);
+?>
