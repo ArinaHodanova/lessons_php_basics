65cda6488b65303746b402bfc6242a29eb2422d8
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-13 16:48:49 +0300

    Update function.php

--- 005sesion/function.php
+++ 005sesion/function.php
@@ -16,7 +16,7 @@ $name = htmlspecialchars(trim($_POST['name']));
 $masseg = htmlspecialchars(trim($_POST['masseg']));
 
 //проверяем поля на заполненность, выводим предупреждение, если поля не заполнены
-if(strlen($name) < 1) {
+if(strlen($name) < 1 ) {
   $_SESSION['error_user_name'] = 'Заполните ваше имя';
 } elseif(strlen($masseg) < 1) {
   $_SESSION['error_masseg'] = 'Сообщение недолжно быть путым';
@@ -24,23 +24,11 @@ if(strlen($name) < 1) {
   $_SESSION['done_mass'] = 'Вы успешно отправили письмо';
 }
 
-//добавляем ваность сообщения
-if(!empty($_POST)) {
+if(!empty($name) && !empty($masseg)) {
   if(!isset($_POST['checkbox'])) {
     $_POST['checkbox'] = 'off';
   } 
   $checkbox = $_POST['checkbox'];
   $_SESSION['arr'][] = ['name'=> $name, 'masseg' => $masseg, 'check' => $checkbox];
-}
-
-//добавлем в маccив имена пользователей 
-$_SESSION['user_name'] = [];
-foreach($_SESSION['arr'] as $elem) {
-  if(!array_key_exists($elem, $_SESSION['user_name'])) $_SESSION['user_name'][$elem['name']] = 0;
-  $_SESSION['user_name'][$elem['name']]++;
-}
-
-foreach($_SESSION['user_name'] as $key => $elem) {
-  echo $key . '<br>';
 }
 ?>
