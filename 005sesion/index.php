f09044484566a09e4f86ffc17c2a5ddf57377261
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-13 14:38:17 +0300

    Update index.php

--- 005sesion/index.php
+++ 005sesion/index.php
@@ -1,60 +1,72 @@
 <?php
+<?
+//error_reporting(-1);
 header("Content-type: text/html; charset=utf-8");
-error_reporting(-1);
 session_start();
-
-if(!empty($_POST)) {
-  $name = $_POST['name'];
-  $masseg = $_POST['masseg']; 
-  if(!isset($_POST['checkbox'])) {
-    $_POST['checkbox'] = 'off';
-  } 
-  $checkbox = $_POST['checkbox'];
-}
-
-$_SESSION['arr'][] = ['name'=> $name, 'masseg' => $masseg, 'check' => $checkbox];
-
-echo '<pre>';
-echo 'SESSION ';
-print_r( $_SESSION);
-echo '</pre>';
-
-echo '<pre>';
-echo 'POST ';
-print_r($_POST);
-echo '</pre>';
+require_once 'function.php';
 
 ?>
+<html lang="ru">
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-<form action="" method="post">
-  <p><input type="text" name="name" placeholder="Ваше имя"></p>
-  <p><input type="text" name="masseg" placeholder="Ваше сообщение"></p>
+<form action="" method="post" class="form">
+  <?
+    if($_SESSION['done_mass']) {
+      echo '<p class="mass">' . $_SESSION['done_mass'] . '</p>';
+    }
+    ?>
 
-  <p>
+  <div class="form_field">
+    <input type="text" name="name" placeholder="Ваше имя">
+    <span class=""><?=$_SESSION['error_user_name']?></span>
+  </div>
+
+  <div class="form_field">
+    <textarea type="text" name="masseg" placeholder="Ваше сообщение"></textarea>
+    <span><?=$_SESSION['error_masseg']?></span>
+  </div>
+
+  <div>
     <input type="checkbox" name="checkbox" id="check1" value="on">
     <label for="check1">Это важное сообщение</label>
-  </p>
+  </div>
 
   <button type="submit" name="submit" value="on">Отправить</button>
 </form>
 
 
+<div class="masseg__row">
 <?if(!empty($_SESSION['arr'])):?>
   <?for($i = 0; $i < count($_SESSION['arr']); $i++):?>
-    <?if($_SESSION['arr'][$i]['check'] !== 'on'):?>
-      <div>
-        <span><?=$_SESSION['arr'][$i]['name']?></span>
-        <span><?=$_SESSION['arr'][$i]['masseg']?></span>
-      </div>
+  
+    <?if($_SESSION['arr'][$i]['check'] == 'on'):?>
+      <div class="masseg__inner bold">
     <?else:?>
-      <div>
-        <span><b><?=$_SESSION['arr'][$i]['name']?></b></span>
-        <span><b><?=$_SESSION['arr'][$i]['masseg']?></b></span>
-      </div>
+      <div class="masseg__inner normal">
     <?endif?>
-    <hr>
-  <?endfor?>
-<?endif?>
 
-<!--<p><a href="s2.php" target="black">Перейти на s2.php</a></p>-->
+    <!--Сообщение без имени выводится не будет, так как имя обязательный парамерт-->
+    <?if($_SESSION['arr'][$i]['name'] == '' || !isset($_SESSION['arr'][$i]['name'])):?>
+      <span class="masseg__inner-error"><?='Имя является обязательным параметром';?></span>
+    <?else:?>
+      <p class="masseg__inner-nam">
+        <span class="masseg__inner-name">Ваше имя: </span>
+        <a href="user_page.php" target="_blank"><span><?=$_SESSION['arr'][$i]['name']?></span></a>
+      </p>
+          <?if($_SESSION['arr'][$i]['masseg'] == '' || !isset($_SESSION['arr'][$i]['masseg'])):?>
+          <span class="masseg__inner-error"><?='Нет сообщения';?></span>
+            <?else:?>
+            <p class="masseg__inner-nam">
+              <span class="masseg__inner-name">Ваше сообщение: </span>
+              <span><?=$_SESSION['arr'][$i]['masseg']?></span>
+            </p>
+          <?endif?>
+    <?endif?>
+      </div><!--/masseg__inner-->
+
+  <?endfor?>
+  <?endif?>
+</div>
 ?>
