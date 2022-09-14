a5e810b273d8804086785c9415860cf8bdd990a1
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-14 17:50:21 +0300

    Update function.php

--- 005sesion/function.php
+++ 005sesion/function.php
@@ -7,6 +7,7 @@ unset($_SESSION['warning']);
 unset($_SESSION['done_mass']);
 
 function redirect() {
+  sleep(10);
   header('Location: index.php');
   exit;
 }
@@ -19,11 +20,12 @@ $masseg = htmlspecialchars(trim($_POST['masseg']));
 if(strlen($name) < 1 ) {
   $_SESSION['error_user_name'] = 'Заполните ваше имя';
 } elseif(strlen($masseg) < 1) {
-  $_SESSION['error_masseg'] = 'Сообщение недолжно быть путым';
+  $_SESSION['error_masseg'] = 'Сообщение не должно быть путым';
 } else {
   $_SESSION['done_mass'] = 'Вы успешно отправили письмо';
 }
 
+//если поля не пустые, то создаем массив с сессией 
 if(!empty($name) && !empty($masseg)) {
   if(!isset($_POST['checkbox'])) {
     $_POST['checkbox'] = 'off';
@@ -31,4 +33,7 @@ if(!empty($name) && !empty($masseg)) {
   $checkbox = $_POST['checkbox'];
   $_SESSION['arr'][] = ['name'=> $name, 'masseg' => $masseg, 'check' => $checkbox];
 }
+
+//получаем все сообщения одного пользователя. 
+$_SESSION['one_user'][$name][] = $masseg;
 ?>
