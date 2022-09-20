4b55a19223b01527baeb110aa15f3029024a020d
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-09-20 16:41:03 +0300

    Update user_page.php

--- 005sesion/user_page.php
+++ 005sesion/user_page.php
@@ -3,4 +3,14 @@ session_start();
 ?>
 <h1><?=$_GET['name']?></h1>
 
+<?
+foreach($_SESSION['one_user'] as $key => $elem) {
+  if($_GET['name'] == $key) {
+    for($i = 0; $i <= count($elem); $i++) {
+      echo $elem[$i] . '<br>';
+    }
+  }
+}
+?>
+
 <a href="index.php">Вернуться в чат</a>
