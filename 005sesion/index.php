ffa63a606a41fa1d269f7bdc3345d260b2678291
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-10-03 10:09:22 +0300

    Update index.php

--- 005sesion/index.php
+++ 005sesion/index.php
@@ -35,32 +35,6 @@ session_start();
   <button type="submit" name="submit" value="on" class="btn">Отправить</button>
 </form>
 
-<div class="message__row">
-<?if(!empty($_SESSION['one_user'])):?>
-<?foreach($_SESSION['one_user'] as $key => $elem):?>
-  <?$amount = count($elem)?>
-
-  <div class="message__inner normal bold">
-    <div class="message__info">
-    <p class="message__inner-nam">
-      <span class="message__inner-name">Имя: </span>
-      <a href="user_page.php?name=<?=$key?>"><?=$key?></a>
-      <!--.Педедаем имя гет параметром-->
-      <span class="message__inner-amount">(<?=$amount?> шт.)</span>
-    </p>
-    <p class="message__inner-nam">
-      <span class="message__inner-name">Сообщение: </span>
-      <span class="message__inner-massage">
-        <!--Выводим только последнее сообщение-->
-        <?$value = end($elem); echo $value?>
-      </span>
-    </p>
-    
-    <a class="message__inner-link" href="user_page.php?name=<?=$key?>">Посмотреть все сообщения пользователя - <?=$key?></a>
-    </div>
-    <a class="drop_messages" href="index.php?val=<?=$val?>">х</a>
-  </div><!--/masseg__inner-->
-
-<?endforeach?>
-<?endif?>
-</div>
+<?
+require_once 'lastWord.php';
+?>
