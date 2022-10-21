a2d29eb8682dce6147702f357af375494ad7bee3
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-10-21 17:30:45 +0300

    Update db.php
    Название таблицы выносим в переменную

--- 011form_registration_and_authorization/db.php
+++ 011form_registration_and_authorization/db.php
@@ -5,11 +5,13 @@ define('DB_NAME', 'training');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
+//Название таблиц
+$users_reg_table = `users_reg`;
+
 try {
   $db = new PDO(DB_DRIVER.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
   print $e->getMessage(); 
 }
-
 ?>
