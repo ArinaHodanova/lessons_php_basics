ef0367bf934479c10a39bc6b2cd561da78e0476d
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-10-25 15:02:12 +0300

    Create edit_user.php
    1. Работаем с данными из формы. Получаем id пользователя, чей профиль редактируем. 
    2. В функцию передаем все полученные данный из $_POST и изменяем информацию пользователя в БД
    3. Формируем сессию с сообщение – “Профиль успешно обновлен”
    4. Перекидываем на страницу пользователя

---
+++ 011form_registration_and_authorization/edit_user.php
@@ -0,0 +1,24 @@
+<?error_reporting(-1);
+require_once __DIR__ . '/function.php';
+require_once __DIR__ . '/db.php';
+
+if(isset($_POST['name_user'])) {
+  $user_name = $_POST['name_user'];
+}
+if(isset($_POST['work_user'])) {
+  $user_work = $_POST['work_user'];
+}
+if(isset($_POST['tel_user'])) {
+  $user_tel = $_POST['tel_user'];
+}
+if(isset($_POST['adress_user'])) {
+  $user_adress = $_POST['adress_user'];
+}
+
+$users = getUsers($db, $users_list_table);//получаем массив пользователей
+$id = getUserById($users);//получаем id пользователя, чей профиль редактируется
+
+editInformation($user_name, $user_work, $user_tel, $user_adress, $id, $db, $users_list_table);
+setFlashMassege('danger', 'Профиль упешно обновлен');
+redirect_to('user_profil.php');
+?>
