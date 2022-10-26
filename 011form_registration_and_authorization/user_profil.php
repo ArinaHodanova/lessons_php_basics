8db140ceae847a965d86d43df20eaad3a577e545
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-10-26 13:03:30 +0300

    Update user_profil.php
    1. Если пользователь не залогинен перебрасываем его на страницу авторизации
    2. Если пользователь не админ и заходит не на свой профиль, то перебрасываем его на страничку с пользователями
    3. Если пользователь  админ и заходит не на свой профиль, то создаем функцию, в которой получаем профиль пользователя, на которой зашли
    4. Владельцу профиля, не зависимо от того админ он или нет, показываем свой профиль. Админа уведомляем о том, что это его профиль

--- 011form_registration_and_authorization/user_profil.php
+++ 011form_registration_and_authorization/user_profil.php
@@ -1,6 +1,37 @@
 <?error_reporting(-1);
 require_once __DIR__ . '/function.php';
 require_once __DIR__ . '/db.php';
+
+//если пользователь не авторизован то перенаправляем на страницу авторизации
+if(isNotLoggedIn()) {
+  redirect_to('login.php');
+}
+
+$users = getUsers($db, $users_list_table);//получаем массив пользователей
+
+/*echo '<pre>';
+print_r($users);
+echo '</pre>';*/
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+//если пользователь админ и автор 
+if(isAdmin(getAuthenticatedUser()) && isAvtor($users, $_GET['id'])) {
+  setFlashMassege('danger', 'Это ваш профиль');
+}
+
+//если пользователь админ и открывает не свой профиль 
+if(isAdmin(getAuthenticatedUser()) && !isAvtor($users, $_GET['id'])) {
+  $users_one = gitUserProfil($users, $_GET['id']);
+}
+
+//если пользователь не админ и открывает не свой профиль
+if(!isAdmin(getAuthenticatedUser()) && !isAvtor($users, $_GET['id']) ) {
+  setFlashMassege('error', 'Это не ваш профиль');
+  redirect_to('users.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -21,11 +52,58 @@ require_once __DIR__ . '/db.php';
 </head>
 <body>
   <main class="page-content">
-    <h1>Профиль пользователя</h1>
-
     <!--если админ редактирует свой профиль-->
     <?if(!empty($_SESSION['danger'] )):?>
-      <div><?=displayFlashMassege('danger')?></div>
+      <div class="alert alert-primary col-xl-4" role="alert"><?=displayFlashMassege('danger')?></div>
     <?endif?> 
+
+    <?if(!empty($_SESSION['error'] )):?>
+      <div class="alert alert-danger col-xl-4" role="alert"><?=displayFlashMassege('error')?></div>
+    <?endif?> 
+
+  <?//показываем профиль владельцу ?>
+  <?if(isAvtor($users, $_GET['id'])):?>
+  <?foreach($users as $user):?>
+    <?if(isIdentical($user, getAuthenticatedUser())):?>
+    <h1>Профиль: <?=$user['username']?></h1>
+
+    <div class="row">
+      <div class="col-md-6">
+        <div style="width: 100px; height: 100px;">
+          <img style="width: 100%" src="<?=$user['image']?>">
+        </div>
+        <div>
+          <h2>Имя: <?=$user['username']?></h2>
+          <p>Работа: <?=$user['job_title']?></p>
+          <p>Телефон: <?=$user['phone']?></p>
+          <p>Почта: <?=$user['email']?></p>
+          <?if(!empty($user['vk']) || !empty($user['telegram']) || !empty($user['instagram'])):?>
+            <h3>Социальные сети</h3>
+          <?endif?>
+          <?if(!empty($user['vk'])):?>
+            <ul>
+              <li><?=$user['vk']?></li>
+              <li><?=$user['telegram']?></li>
+              <li><?=$user['instagram']?></li>
+            </ul>
+          <?endif?>
+        </div>
+      </div>
+    </div>
+    <?endif?>
+  <?endforeach?>
+  <?endif?>
+
+  <?//если пользователь админ и не автор?>
+  <?if(isset($users_one )):?>
+    <?
+     echo '<pre>';
+     print_r($users_one );
+     echo '<pre>'; 
+    ?>
+  <?endif?>
+
+
+  <a href="users.php">Вернуться к списку пользователей</a>
   </main>
 </body>
