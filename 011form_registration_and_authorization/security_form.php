43d9ec38d5f13849079550385c8799d12a8f177a
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-10-27 15:05:50 +0300

    Update security_form.php
    1. Пароль и почту нужно изменить в обеих таблицах. Для этого я получила по id почту пользователя, которую нужно изменить.  По почте получила id пользователя из другой таблицы. 
    2. В функцию по редактированию информации передаю id пользователя. И меняю информацию в двух таблицах  
    3. Если пользователь админ и редактирует не свой профиль, то перебрасываем на страничку с пользователями и сообщаем об успешном обновлении информации.  В остальных случая перекидываем на авторизацию

--- 011form_registration_and_authorization/security_form.php
+++ 011form_registration_and_authorization/security_form.php
@@ -3,24 +3,42 @@ require_once __DIR__ . '/function.php';
 require_once __DIR__ . '/db.php';
 
 $id_page = $_GET['id'];
-$users = getUsers($db, $users_list_table);//получаем массив пользователей
+$users_lt = getUsers($db, $users_list_table);//получаем массив пользователей
+$users_rt = getUsers($db, $users_reg_table);//получаем массив пользователей
 
 $email = $_POST['email'];
 $password = $_POST['password'];
-
-echo '<pre>';
-print_r($_SESSION['user']);
-echo '</pre>';
 
 //проверяем что бы поля не были пустыми 
 checkfFieldEmptiness($email, $password, null, 'security.php?id='.$id_page);
 
 //получаем id пользователя
-$id_users = getUserById($users);
+$id_users_list_table = getUserById($users_lt);
 
 //проверить занят ли емайл и не совпадает ли введеная почта с почтой профиля который редактирую
-if(getUzerByEmail($email, $db, $users_reg_table) && !isOwnerProfile($users, $id_page, $email)) {
+if(getUzerByEmail($email, $db, $users_reg_table) && !isOwnerProfile($users_lt, $id_page, $email)) {
   setFlashMassege('error', 'Данный почтовый адрес уже занят');
   redirect_to('security.php?id='.$id_page);
 }
-echo $id_users;
+
+//получаем емайл пользователя, по которому получим id того же пользователя из другой таблицы
+getUserByEmail($users_lt, $id_users_list_table);
+//получаем id пользователя из второй таблицы
+foreach($users_rt as $user) { 
+  if(getUserByEmail($users_lt, $id_users_list_table) == $user['email']) {
+    $id_users_reg_table = $user['id'];
+  }
+}
+
+//изменяем данные
+editCreadentials($email, null, $db, $id_users_list_table, $users_list_table);
+editCreadentials($email, $password, $db, $id_users_reg_table, $users_reg_table);
+
+if(isAdmin(getAuthenticatedUser()) && !isOwnerProfile($users_lt, $id_page, $email)) {
+  setFlashMassege('success', 'Данные упешно обновлены');
+  redirect_to('users.php');
+}
+
+setFlashMassege('success', 'Данные упешно обновлены');
+redirect_to('login.php');
+?>
