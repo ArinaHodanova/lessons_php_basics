5c8b0a9b66460eee60d3b7403b2e653a64bf27dc
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-10-28 16:55:59 +0300

    Update users.php

--- 011form_registration_and_authorization/users.php
+++ 011form_registration_and_authorization/users.php
@@ -52,7 +52,7 @@ $users = getUsers($db);//получаем массив пользователе�
                     <div id="c_1" class="card border shadow-0 mb-g shadow-sm-hover" data-filter-tags="oliver kopyov" <?=(isIdentical($user, getAuthenticatedUser()))? "style=background-color:#ffeaea;": false ?>>
                         <div class="card-body border-faded border-top-0 border-left-0 border-right-0 rounded-top">
                             <div class="d-flex flex-row align-items-center">
-                                <span class="status status-success mr-3">
+                                <span class="status status-<?=$user['status']?> mr-3">
                                     <span class="rounded-circle profile-image d-block " style="background-image:url(<?=$user['image']?>); background-size: cover;"></span>
                                 </span>
                                 <div class="info-card-text flex-1">
