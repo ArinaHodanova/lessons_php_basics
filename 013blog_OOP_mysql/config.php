68f4b43b7c90131535a300ccb0bf803b183a2fe2
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-07 16:46:24 +0300

    Create config.php
    Информацию о БД, пароль и логин выносим в данный файл и возвращаем ассоциативным массивом

---
+++ 013blog_OOP_mysql/config.php
@@ -0,0 +1,11 @@
+<?
+return [
+  'database' => [
+    'connect' => 'mysql:host=localhost',
+    'db' => 'users_db',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+  ],
+]
+?>
