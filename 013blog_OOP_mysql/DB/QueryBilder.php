c80a06f7ed4fccd924a064117bfa4e74c1dbc2e0
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-09 14:15:18 +0300

    Update QueryBilder.php
    1. Работаем над методом create. Метод позволяет осуществить SQL запрос “INSERT INTO table (namefield) VALUES ('val')” для любой таблицы и любого кол-ва полей в данной таблице.
    2. метод getOne для вывода отдельной записи по id.
    3. метод update. Работаем как с таблицей, где нужно редактировать одно поле и множество полей.

--- 013blog_OOP_mysql/DB/QueryBilder.php
+++ 013blog_OOP_mysql/DB/QueryBilder.php
@@ -8,11 +8,44 @@ class QueryBilder {
   }
 
   public function getAll($table) {
-    //запрос
     $stat = $this->pdo->prepare("SELECT * FROM {$table}");
     $stat->execute();
     return $stat->fetchAll(PDO::FETCH_ASSOC);
   }
-}
 
+  public function create($table, $data) {
+    $keys = implode(', ', array_keys($data));
+    $tags = ":" . implode(', :', array_keys($data));
+
+    $sql = "INSERT INTO {$table} ({$keys}) VALUES ({$tags})";
+    $stat = $this->pdo->prepare($sql);
+    $stat->execute($data);
+    dd($stat);
+  }
+
+  public function getOne($table, $id) {
+    $sql = "SELECT * FROM {$table} WHERE id=:id";
+    $stat = $this->pdo->prepare($sql);
+    $stat->bindValue(':id', $id);
+    $stat->execute();
+    return $stat->fetch(PDO::FETCH_ASSOC);
+  }
+
+  public function update($table, $data, $id) {
+    dd($data);
+    $keys = array_keys($data);
+
+    foreach ($keys  as $key => $value) {
+      $str .= $value . ' = :' . $value . ',';
+    }
+    $keys = rtrim($str, ',');
+    $data['id'] = $id;
+    
+    $sql = "UPDATE `{$table}` SET {$keys} WHERE id=:id";
+    $stat = $this->pdo->prepare($sql);
+    $stat->execute($data);
+    dd($stat);
+  }
+
+}
 ?>
