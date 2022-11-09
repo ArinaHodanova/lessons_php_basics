1eafc535b83737c1399a8de5f1f6862ba6fa4db3
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-09 15:30:46 +0300

    Update update.php

--- 013blog_OOP_mysql/update.php
+++ 013blog_OOP_mysql/update.php
@@ -2,7 +2,10 @@
 require_once 'functions.php';
 $db = require_once 'DB/start.php';//подключаемся к БД
 
-$post = $db->update('posts', $_POST, $_GET['id']);
+$post = $db->update('posts', [
+    'title' => $_POST['title']
+  ],
+$_GET['id']);
 
 header('Location: index.php');
 ?>
