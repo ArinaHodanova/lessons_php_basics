998e79590425b4c32470c1748b396008981c1355
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-09 14:34:57 +0300

    Create create.php

---
+++ 013blog_OOP_mysql/create.php
@@ -0,0 +1,24 @@
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Create Post</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 offet-md-2">
+            <form class="" action="store.php" method="post">
+              <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" class="input-group mb-3"> 
+              </div>
+              <div class="form-grop">
+                <button type="submit" class="btn btn-success">Add Post</button>
+              </div>
+            </form>
+        </div>
+      </div>
+    </body>
+</html>
