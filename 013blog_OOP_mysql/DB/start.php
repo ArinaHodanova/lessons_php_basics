6bc7fad4a33638b01676af5dff9ff62cfd8e9f50
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-14 12:58:12 +0300

    Update start.php

--- 013blog_OOP_mysql/DB/start.php
+++ 013blog_OOP_mysql/DB/start.php
@@ -1,7 +1,7 @@
 <?
-$config = require_once 'config.php';
-require_once 'DB/Connect.php';
-require_once 'DB/QueryBilder.php';
+$config = include __DIR__ . '/../config.php';
+include __DIR__ . '/../DB/Connect.php';//подключение к БД
+include __DIR__ . '/../DB/QueryBilder.php';//выполнение запроса
 
 return new QueryBilder(
   Connect::make($config['database'])
