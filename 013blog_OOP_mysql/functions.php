df05189d47a7a392e02bb3de0882e84d85eb3190
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-14 17:39:32 +0300

    Update functions.php

--- 013blog_OOP_mysql/functions.php
+++ 013blog_OOP_mysql/functions.php
@@ -4,4 +4,9 @@ function dd($add) {
   var_dump($add);
   echo '</pre>';
 }
+
+function getPath($path) {
+  $position = strpos($path, '?');
+  return $position === false ? $path : substr($path, 0, $position);
+}
 ?>
