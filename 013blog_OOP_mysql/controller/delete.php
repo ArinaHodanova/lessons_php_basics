263825450ef55cf130a0c290494768220c1176e7
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-14 17:34:56 +0300

    Update delete.php

--- 013blog_OOP_mysql/controller/delete.php
+++ 013blog_OOP_mysql/controller/delete.php
@@ -1,8 +1,6 @@
 <?
-echo 'lf';
 $db = include __DIR__ . '/../DB/start.php';//подключаемся к БД
 
 $db->delete('posts', $_GET['id']);
-echo 'Да';
-//header('Location: /');
+header('Location: /');
 ?>
