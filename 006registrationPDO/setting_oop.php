d478010d16780cd5e3ed5169f7e465fa82f3b3e2
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-15 17:44:30 +0300

    Update setting_oop.php

--- 006registrationPDO/setting_oop.php
+++ 006registrationPDO/setting_oop.php
@@ -14,27 +14,4 @@ $statment = $db->prepare($sql_select);
 $statment->execute(['email' => $email]);
 $result = $statment->fetch(PDO::FETCH_ASSOC);
 
-//старый код 
-var_dump($_SESSION);
-
-//если емайл существует, то выводим предупредением
-if(!empty($result)) {
-  //$_SESSION['result'] = 'Данный email уже есть';
-}
-
-//проверяем заполнены ли поля email
-if(empty($email)) {
-  //$_SESSION['error_email'] = 'Установите email';
-}
-//проверяем заполнены ли поля пароль
-if(empty($password)) {
-  //$_SESSION['error_password'] = 'Установите пароль';
-}
-
-$sql_insert = "INSERT INTO `users_reg` (`email`, `password`) VALUES (:email, :password)";
-$statment = $db->prepare($sql_insert);
-$statment->execute(['email' => $email, 'password' => $password,]);
-
-
-
 ?>
