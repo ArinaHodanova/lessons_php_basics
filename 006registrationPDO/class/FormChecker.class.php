b9ffc0fc9d7907510c3cd9fea207d50245b28f3e
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-15 17:38:41 +0300

    Update FormChecker.class.php

--- 006registrationPDO/class/FormChecker.class.php
+++ 006registrationPDO/class/FormChecker.class.php
@@ -1,5 +1,11 @@
 <?
 class FormChecker {
-  
+    private $name;
+    private $mess;
+    protected $is_empty_email = false;
+
+    public function getIsEmptyEmail() {
+      return $this->is_empty_email;
+    } 
 }
 ?>
