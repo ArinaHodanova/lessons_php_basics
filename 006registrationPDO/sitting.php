e1d5d88d0722e31edfc5a00290fd07f7b208d02f
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-15 14:58:44 +0300

    Update sitting.php

--- 006registrationPDO/sitting.php
+++ 006registrationPDO/sitting.php
@@ -5,7 +5,7 @@ $password = $_POST['password'];
 
 var_dump($_POST);
 //подключаемся к БД
-$db = new PDO('mysql:host=localhost;dbname=#', '#', '#');
+$db = new PDO('mysql:host=localhost;dbname=training', 'root', 'root');
 //делаем запрос, который проверяет если ли значения поля в БД совпад за значение поля формы
 $sql_select = "SELECT * FROM `users_reg` WHERE email=:email";
 $statment = $db->prepare($sql_select);
@@ -16,20 +16,20 @@ $result = $statment->fetch(PDO::FETCH_ASSOC);
 //если емайл существует, то выводим предупредением
 if(!empty($result)) {
   $_SESSION['result'] = 'Данный email уже есть';
-  header("Location: /1/index.php");
+  header("Location: /");
   exit;
 }
 
 //проверяем заполнены ли поля email
 if(empty($email)) {
   $_SESSION['error_email'] = 'Установите email';
-  header("Location: /1/index.php");
+  header("Location: /");
   exit;
 }
 //проверяем заполнены ли поля пароль
 if(empty($password)) {
   $_SESSION['error_password'] = 'Установите пароль';
-  header("Location: /1/index.php");
+  header("Location: /");
   exit;
 }
 
@@ -40,5 +40,5 @@ $statment->execute(['email' => $email, 'password' => $password,]);
 $massage = 'Данные отправлены';
 $_SESSION['success'] = $massage;
 
-header("Location: /1/index.php");
+header("Location: /");
 ?>
