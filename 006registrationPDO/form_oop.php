0375964c41a8ced10b13d6dfb899204970cdd131
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-16 14:51:51 +0300

    Create form_oop.php
    Рефакторинг кода.  Данный файл содержит идентичную по содержанию и виду форму , как в файле form.php, но обрабатываться код будет подходим ООП.

---
+++ 006registrationPDO/form_oop.php
@@ -0,0 +1,12 @@
+<form action="setting_oop.php" method="post" class="form-group">
+    <p>Подход: ООП</p>
+        <div class="form-input">
+            <label class="form-label" for="simpleinput">Email</label>
+            <input type="text" name="email" id="simpleinput">
+        </div>
+        <div class="form-input">
+            <label class="form-label" for="simpleinput">Password</label>
+            <input type="password" name="password" id="simpleinput">
+        </div>
+    <button type="submit" class="btn">Отправить</button>
+</form>
