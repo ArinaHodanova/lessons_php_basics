6034ac84999a5f2e0f88b6e5df731340acd71df1
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-18 15:14:34 +0300

    Create index.php
    Юзаем работу методов

---
+++ 012components_OOP/index.php
@@ -0,0 +1,45 @@
+<?
+session_start();
+$config = require_once 'config.php';
+include 'function.php';
+require_once 'Database.php';
+
+//Вывести список
+//$users = Database::getMake()->request('SELECT * FROM people');
+
+//Выполняем запрос с передачей и получанием одного параметра 
+//$users = Database::getMake()->request('SELECT * FROM people WHERE fname = ?', ['Иванова']);
+
+//Выполняем запрос с передачей и получанием множества параметров
+//$users = Database::getMake()->request('SELECT * FROM people WHERE fname IN (?, ?, ?)', ['Николаева','Иванова', 'Юданова']);
+
+/**
+ * Запрос get где не используется sql запрос, а используется обертка
+ * Первый параметр - название таблицы
+ * Второй парамерт - поле , оператор, значение
+*/
+//$users = Database::getMake()->get('people' , ['id', '>' , '0'])->result();
+//dd($users);
+
+/**
+ * Запрос delete где не используется sql запрос, а используется обертка
+ * Первый параметр - название таблицы
+ * Второй парамерт - поле , оператор, значение
+*/
+//Database::getMake()->delete('people' , ['id', '=' , '16']);
+
+
+/**
+ * Запрос insert где не используется sql запрос, а используется обертка
+ * Первый параметр - название таблицы
+ * Второй парамерт - многомерный массив если нуно ставить насколько значений и одномерный массив, если нужно вставить одно значение
+*/
+/*$users_set = Database::getMake()->insert('people' , [
+      'name' => 'Иван',
+      'fname' => 'Редьков',
+    ]
+);*/
+
+$users = Database::getMake()->request('SELECT * FROM people');
+dd($users->result());
+?>
