714b7b99c8677beab1962db6a8f7398ffac3f3a2
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-21 12:31:26 +0300

    Create conf.php
    Данные в виде ассоц. массива

---
+++ 012components_OOP/conf.php
@@ -0,0 +1,19 @@
+<?
+require_once 'Config.php';
+require_once 'Database.php';
+
+$GLOBALS['config'] = [
+  'mysql' => [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'db' => 'users_db',
+    'something' => [
+      'no' => [
+        'no' => 'goot'
+      ]
+    ]
+  ],
+];
+
+?>
