2c77da7a8fcf1a41a0c0cdabafdfad1b6650a793
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-22 22:29:38 +0300

    Update connect.php

--- 006registrationPDO/class/connect.php
+++ 006registrationPDO/class/connect.php
@@ -1,7 +1,7 @@
 <?
 $config = include __DIR__ . '/../conf.php';
 $db = include __DIR__ . '/Connect.class.php';//подключение к БД
-include __DIR__ . '/FormChecker.class.php';
+include __DIR__ . '/Validator.class.php';
 include __DIR__ . '/QueryBilder.class.php';
 
 $query = new QueryBilder (
