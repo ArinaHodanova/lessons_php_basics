7db9d3dd7e8b41e55ee1bcb27071c2abe68bef06
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-23 12:10:26 +0300

    Create setting.php
    Настройки формы. При успешном прохождении валидации формирую сообщение и вывожу пока в файле - admin_panel

---
+++ 012components_OOP/setting.php
@@ -0,0 +1,41 @@
+<?
+session_start();
+require_once 'function.php';
+require_once 'conf.php';
+
+//обработчик формы
+if(Input::exists()) {
+  //значение токина совпадает со значение пользователя 
+  if(Token::check(Input::get('token'))) {
+    $validate = new Validate();
+
+    $validation = $validate->check($_POST, [
+      'email' => [
+        'required' => true,
+        'min' => 2,
+        'max' => 30,
+        'validate_email' => true,
+        'unique' => 'users_reg',//уникальный емайл в таблице
+      ],
+      'password' => [
+        'required' => true,
+        'min' => 3
+      ],
+      'password_again' => [
+        'required' => true,
+        'matches' => 'password'
+      ]
+    ]);
+
+    if($validate->passed()) {
+        Session::flash('success', 'register success');
+        header('Location: admin_panel.php');
+    } else {
+      foreach($validate->errors() as $error) {
+        echo $error . '<br>';
+      }
+    }
+
+  }
+}
+?>
