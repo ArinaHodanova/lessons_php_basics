753890f4f20b6ce088b7b7480a736e47d14d357d
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-25 17:08:27 +0300

    Update User.php
    Добавила возможность вытащить пользователя по id и вытащить залогиненого пользователя

--- 012components_OOP/class/User.php
+++ 012components_OOP/class/User.php
@@ -1,10 +1,24 @@
 <?
 class User {
-  private $db, $data, $session_name;
+  private $db, $data, $session_name, $isLoggedIn;
 
-  public function __construct() {
+  public function __construct($user = null) {
     $this->db = Database::getMake();
     $this->session_name = Config::get('session.user_session');
+
+    if(!$user) {
+      //если сессия существует, то вытаскиваем пользователя по id
+      if(Session::exists($this->session_name)) {
+        $user = Session::get($this->session_name);//id текущего пользователя
+          if($this->find($user)) {
+            $this->isLoggedIn = true;
+          } else {
+            //
+          }
+      }
+    } else {
+      $this->find($user);
+    }
   }
 
   public function create($fields = []) {
@@ -18,11 +32,7 @@ class User {
 
       if($user) {
         if(password_verify($password, $this->data()->password)) {
-          Session::put($this->session_name , 
-            [
-              'id' => $this->data()->id,
-              'username' => $this->data()->username
-            ]);
+          Session::put($this->session_name, $this->data()->id);
           return true;
         }
       }
@@ -31,8 +41,13 @@ class User {
   }
 
   //метод для нахождения пользователя
-  public function find($email = null) {
-    $this->data = $this->db->get('users_reg', ['email', '=', $email])->first();
+  public function find($value = null) {
+    if(is_numeric($value)) {
+        $this->data = $this->db->get('users_reg', ['id', '=', $value])->first();
+    } else {
+        $this->data = $this->db->get('users_reg', ['email', '=', $value])->first();
+    }
+    
     if($this->data) {
       return true;
     }
@@ -42,6 +57,10 @@ class User {
   //геттер для получения данных, которые мы записали
   public function data() {
     return $this->data;
+  }
+
+  public function isLoggedIn() {
+    return $this->isLoggedIn;
   }
 }
 ?>
