0a609cdf58f646c0050466512c6aec62d9b3f094
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-29 09:45:57 +0300

    Update setting_oop.php

--- 006registrationPDO/setting_oop.php
+++ 006registrationPDO/setting_oop.php
@@ -1,10 +1,9 @@
 <?
+session_start();
 include  __DIR__ . '/class/connect.php';
 $form_check = new Validator();
 
-session_start();
 $_SESSION['form_check'] = $form_check;
-
 
 header('Location: index.php');
 ?>
