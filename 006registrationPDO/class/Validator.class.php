ee7ec86ed071e8478d74dca61e85922d21b8b3a6
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-30 15:12:36 +0300

    Update Validator.class.php
    Добавление метода, и свойства показывающего состояние поля с паролем

--- 006registrationPDO/class/Validator.class.php
+++ 006registrationPDO/class/Validator.class.php
@@ -3,21 +3,27 @@ class Validator {
     private $name;
     private $mess;
     protected $is_empty_email = false;
+    protected $is_empty_password =  false;
 
     //ошибка у поля email
     public function check() {
 
       if(empty($_POST['email'])) {
-        echo 'Да';
-        $this->error = true;
-      } else {
-        echo 'Нет';
+        $this->is_empty_email = true;
+      }
+
+      if(empty($_POST['password'])) {
+        $this->is_empty_password = true;
       }
 
     }
 
     public function getIsEmptyEmail() {
         return $this->is_empty_email;
+    } 
+
+    public function getIsEmptyPassword() {
+      return $this->is_empty_password;
     } 
 }
 ?>
