7df4254cbcd8585bf4edc10f74dc1224d12ef834
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-11-30 15:14:08 +0300

    Update form_oop.php

--- 006registrationPDO/form_oop.php
+++ 006registrationPDO/form_oop.php
@@ -1,19 +1,19 @@
 <?
-echo '<pre>';
-var_dump($_SESSION);
-echo '</pre>';
 $val = $_SESSION['validator'];
 ?>
 
 <form action="setting_oop.php" method="post" class="form-group">
     <p>Подход: ООП</p>
         <?if($val->getIsEmptyEmail()):?>
-            <p>Ошибка</p>
+            <p>заполните поле емейл</p>
         <?endif?>
         <div class="form-input">
             <label class="form-label" for="simpleinput">Email</label>
             <input type="text" name="email" id="simpleinput">
         </div>
+        <?if($val->getIsEmptyPassword() ):?>
+            <p>заполните поле пароль</p>
+        <?endif?>
         <div class="form-input">
             <label class="form-label" for="simpleinput">Password</label>
             <input type="password" name="password" id="simpleinput">
