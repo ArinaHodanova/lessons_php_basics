76675f4df24fd87ddcdf41597d0a7a7eacba47ed
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-12-05 15:43:11 +0300

    Create update_profile.php
    Редактирование имени пользователя по id

---
+++ 012components_OOP/update_profile.php
@@ -0,0 +1,46 @@
+<?
+session_start();
+require_once 'function.php';
+require_once 'init.php';
+
+//обработчик формы
+//есть поле не пустое
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+$user = new User; 
+$validate = new Validate();
+$validate->check($_POST , [
+  'username' => [
+    'required' => true,
+    'min' => 2,
+    'max' => 30,
+  ]
+]);
+
+if(Input::exists()) { 
+  if(Token::check(Input::get('token'))) { 
+    if($validate->passed()) {
+      $user->update(['username' => Input::get('username')]);
+    } else {
+        foreach($validate->errors() as $error) {
+          echo $error . '<br>';
+        }
+    }
+  }
+}
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Your name<label>
+        <input type="text" name="username" value="<?=Input::get('username')?>"></input>
+    </div>
+
+    <input type="hidden" name="token" value="<?=Token::generate();?>">
+
+    <div class="field">
+        <button type="submit">Submit</buttin>
+    </div>
+</form>
