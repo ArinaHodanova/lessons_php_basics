021b20bac7ab7d46805ab5cf3478383cde5abc64
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-12-14 14:11:12 +0300

    Update View.php
    Метод для вывода шаблона и видов

--- 014MVC_framework/application/core/View.php
+++ 014MVC_framework/application/core/View.php
@@ -3,18 +3,30 @@ namespace application\core;
 
 class View
 {
-  public $route;
   public $path;//путь к виду
-  public $layout ='defaulte';//шаблон
+  public $route;
+  public $layout ='default';//шаблон
 
   public function __construct($route) 
   {
     $this->route = $route;
     $this->path = $route['controller'].'/'.$route['action'];
-    
-    echo '<b>sdvsdv';
-    debug($this->path);
-    echo '</b>';
   }
+
+  //загружает шаблон и виды
+  public function render($title, $vars = []) 
+  { 
+    if(file_exists('application/views/'.$this->path.'.php')) {
+        ob_start();
+        require 'application/views/'.$this->path.'.php';
+        $content = ob_get_clean();
+        require 'application/views/layouts/'.$this->layout.'.php';
+    } else 
+    {
+        echo 'Вид не найден '.$this->path;
+    }
+
+  }
+
 }
 ?>
