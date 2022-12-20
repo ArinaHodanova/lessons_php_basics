4ed033872cb52f768457eac2158c61a4479908ea
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-12-20 21:46:42 +0300

    Update Controller.php
    Добавление метода для работы с моделью

--- 014MVC_framework/application/core/Controller.php
+++ 014MVC_framework/application/core/Controller.php
@@ -7,12 +7,26 @@ abstract class Controller
 {
   public $route;
   public $view;
+  public $model;
 
   public function __construct($route) 
   {
     $this->route = $route;
     $this->view = new View($route);
+    $this->model = $this->loadModel($this->route['controller']);
+
   }
 
+  public function loadModel($name)
+  {
+    $path = 'application\models\\'.ucfirst($name);
+    if(class_exists($path)) 
+    {
+      return new $path();
+    } else 
+    {
+      echo 'Модель не найдена';
+    }
+  }
 }
 ?>
