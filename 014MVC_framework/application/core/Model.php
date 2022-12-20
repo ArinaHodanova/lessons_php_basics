0eaf3af3b270dde21dac8f33cc3e10a2a71c39d9
ArinaArina <69355804+ArinaHodanova@users.noreply.github.com>
2022-12-20 21:49:01 +0300

    Update Model.php

---
+++ 014MVC_framework/application/core/Model.php
@@ -0,0 +1,15 @@
+<?
+namespace application\core;
+
+use application\lib\Database;
+
+abstract class Model 
+{
+    public $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+}
+?>
